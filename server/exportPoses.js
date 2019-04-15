338cabeb171bc733dd9eb694d97dde9d2959af52
Daniel Waters <danwater1@gmail.com>
2019-04-15 15:28:27 -0700

    Fixes ignoring the .gitKeep files on exporting poses

--- server/exportPoses.js
+++ server/exportPoses.js
@@ -3,21 +3,23 @@ const fs = require("fs");
 const allPoses = [];
 
 function getDirectories(path) {
-  return fs.readdirSync(path).filter(function (file) {
-    return fs.statSync(path+'/'+file).isDirectory();
-  })
+  return fs.readdirSync(path).filter(function(file) {
+    return fs.statSync(path + "/" + file).isDirectory();
+  });
 }
 
 function getPosesInDirectory(path) {
   fs.readdirSync(`poses/${path}`).forEach(file => {
-    allPoses.push(JSON.parse(fs.readFileSync(`poses/${path}/${file}`)));
-  })
+    if (file.endsWith('.json')) {
+      allPoses.push(JSON.parse(fs.readFileSync(`poses/${path}/${file}`)));
+    }
+  });
 }
 
 function exportPoses(path) {
   const directories = getDirectories(path);
   directories.forEach(path => getPosesInDirectory(path));
-  fs.writeFileSync('poseData.json', JSON.stringify(allPoses));
+  fs.writeFileSync("poseData.json", JSON.stringify(allPoses));
 }
 
-exportPoses('poses');
+exportPoses("poses");
